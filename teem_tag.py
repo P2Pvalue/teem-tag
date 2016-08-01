8d9dfee7a53232fd3643f7ed255d34d72d24969d
prastut <kr.prastut@gmail.com>
2016-08-01 10:45:48 +0530

    Added Flask for Webserver support

--- teem_tag.py
+++ teem_tag.py
@@ -3,8 +3,10 @@
 
 from core import tagger
 from pymongo import *
+from flask import Flask
 import os, pickle, pymongo
 import json
+
 
 from bson.objectid import ObjectId 
 
@@ -12,26 +14,36 @@ from bson.objectid import ObjectId
 db_host = os.environ.get('MONGO_PORT_27017_TCP_ADDR')
 db_port = os.environ.get('MONGO_PORT_27017_TCP_PORT')
 
+app = Flask(__name__)
 
+@app.route("/")
+def tags():
+    string = "The P2Pvalue project is mapping the diffusion and hybridization of peer production and investigating the conditions which favour collaborative creation and the logic of value of these emerging forms."
+    data = { 'data' : mytagger(string,5)}
+    return json.dumps(data, default=lambda x: str(x).strip('"\''))
+    
 
-
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     if not(os.path.isfile('data/nltkdict.pkl')):
         build_dict_from_nltk('data/nltkdict.pkl')
-
     
     weights = pickle.load(open('data/nltkdict.pkl'))
     mytagger = tagger.Tagger(tagger.Reader(), tagger.Stemmer(), tagger.Rater(weights))
     
-    client = MongoClient('172.17.0.2',27017)
-    collection = client.swellrt.models
-    
-    cursor = collection.find()
-    key = "description"
+    # client = MongoClient('172.17.0.2',27017)
+    # collection = client.swellrt.models
+    # for doc in cursor:
+    #     root = doc['root']
+    #     if key in root:
+    #         data = {'waveid' : doc['wave_id'],  'waveletid' : doc['wavelet_id'], 'path' : 'xyz', 'data' : mytagger(root[key],10)}
+    #         print json.dumps(data, default=lambda x: str(x).strip('"\''))
 
-    for doc in cursor:
-        root = doc['root']
-        if key in root:
-            data = {'waveid' : doc['wave_id'],  'waveletid' : doc['wavelet_id'], 'path' : 'xyz', 'data' : mytagger(root[key],10)}
-            print json.dumps(data, default=lambda x: str(x).strip('"\''))
+    
+    # cursor = collection.find()
+    # key = "description"
+
+    app.run(debug=True, host='0.0.0.0')
+
+
+
+    
